4ad367aebeaf394fd8e2879f6082c77a02e98348
Alain Horner <alain.hornair@gmail.com>
2021-09-09 19:12:25 +0200

    basic transaction component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,8 @@
       <li>Total Supply: {{ erc20TotalSupply }}</li>
     </ul>
 
+    <NewTransaction v-if="newTransactionData" :web3="web3" v-bind="newTransactionData.account"></NewTransaction>
+
     <div class="max-w-screen-sm shadow overflow-hidden border-b border-gray-200 sm:rounded-lg px-6 py-4 bg-white text-left">
       <h2 class="text-2xl mb-8">Accounts</h2>
 
@@ -36,7 +38,7 @@
             </div>
             <div class="flex-1 flex items-end justify-between text-sm">
               <p class="text-gray-500">
-                Secondary Info
+                <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500" @click="sendFromAccount(index)">Send</button>
               </p>
               <div class="flex">
                 <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500" @click="removeAccount(index)">Remove</button>
@@ -54,26 +56,29 @@
         Add Account
       </button>
     </div>
+
   </div>
 </template>
 
 <script>
 
 import Web3 from 'web3';
-
-const web3 = new Web3(
-  new Web3.providers.HttpProvider('https://kovan.infura.io/v3/39009bec93694f98947fdfb1cffb2e30')
-);
+import NewTransaction from "@/components/NewTransaction";
 
 const erc20Abi = [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
 
 export default {
   name: 'HelloWorld',
+  components: {
+    NewTransaction
+  },
   props: {
     erc20Address: String
   },
   data() {
     return {
+      web3: null,
+      newTransactionData: null,
       erc20Name: '',
       erc20Symbol: '',
       erc20TotalSupply: '',
@@ -92,8 +97,8 @@ export default {
         this.accounts = [...this.accountsRaw];
 
         this.accounts.forEach((account) => {
-          web3.eth.getBalance(account.address, (err, wei) => {
-            account.balance = Number.parseFloat(web3.utils.fromWei(wei, 'ether')).toPrecision(2);
+          this.web3.eth.getBalance(account.address, (err, wei) => {
+            account.balance = Number.parseFloat(this.web3.utils.fromWei(wei, 'ether')).toPrecision(2);
           });
         });
       },
@@ -102,7 +107,7 @@ export default {
   },
   methods: {
     addAccount() {
-      const newAccount = web3.eth.accounts.create();
+      const newAccount = this.web3.eth.accounts.create();
       newAccount.name = `My Account ${this.accountsRaw.length + 1}`;
       this.accountsRaw = [...this.accountsRaw, newAccount];
     },
@@ -110,10 +115,19 @@ export default {
       if (window.confirm('Are you sure? You will loose access to your funds.')) {
         this.accountsRaw = this.accountsRaw.filter((account, accountIndex) => accountIndex !== clickedIndex );
       }
+    },
+    sendFromAccount(clickedIndex) {
+      this.newTransactionData = {
+        account: this.accountsRaw[clickedIndex]
+      }
     }
   },
   created() {
-    const smartContract = new web3.eth.Contract(erc20Abi, this.erc20Address);
+    this.web3 = new Web3(
+        new Web3.providers.HttpProvider('https://kovan.infura.io/v3/39009bec93694f98947fdfb1cffb2e30')
+    );
+
+    const smartContract = new this.web3.eth.Contract(erc20Abi, this.erc20Address);
 
     // get all methods: console.log(smartContract.methods);
     smartContract.methods.name().call((err, result) => (this.erc20Name = result));
---
+++ src/components/NewTransaction.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="max-w-screen-sm shadow overflow-hidden border-b border-gray-200 sm:rounded-lg px-6 py-4 bg-white text-left">
+      <h2 class="text-2xl mb-8">New Transaction</h2>
+
+      <ul>
+        <li>{{ name }}</li>
+        <li>{{ address }}</li>
+        <li>{{ privateKey }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Transaction',
+  props: {
+    web3: null,
+    name: String,
+    address: String,
+    privateKey: String
+  },
+  created() {
+    console.log('hello transaction world');
+  }
+}
+</script>
